9dfba6bc51912f66ffd337679aa0f11a9eb1fe52
kurokeita <kuro.keita94@gmail.com>
2023-12-04 18:08:49 +0700

    add keycloak to the stack

---
+++ scripts/db_restore.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cd ../.mariadb/ || exit
+
+if ! [ -f "mariadb_backup.tar.gz" ]; then
+    echo "There is no backup tar file to restore from."
+
+    exit
+fi
+
+echo "Shutting down mariadb container..."
+docker stop lab.mariadb
+
+echo "Restoring mariadb backup..."
+sudo cp mariadb_backup.tar.gz ../mariadb_backup.tar.gz
+sudo rm -rf ./*
+sudo cp ../mariadb_backup.tar.gz mariadb_backup.tar.gz
+sudo tar xf mariadb_backup.tar.gz
+sudo rm -rf ../mariadb_backup.tar.gz
+echo "Done restoring mariadb backup."
+
+echo "Starting mariadb container..."
+docker start lab.mariadb
