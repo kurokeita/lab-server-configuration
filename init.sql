daef58dffa95a7526484c9ebf07b871c6f7b9375
kurokeita <kuro.keita94@gmail.com>
2023-12-18 00:34:57 +0700

    add paperless

--- init.sql
+++ init.sql
@@ -1,6 +1,9 @@
 -- Create the 'keycloak' database
 CREATE DATABASE IF NOT EXISTS keycloak;
 
+-- Create the 'paperless' database
+CREATE DATABASE IF NOT EXISTS paperless;
+
 -- Grant all privileges on 'keycloak' database to 'lab' user
 GRANT ALL PRIVILEGES ON keycloak.* TO 'lab'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
